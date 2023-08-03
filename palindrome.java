7d6ff4afa5632a7b60c9e41e5d33cb0fff8b3964
Vellaisamy-24 <vellaikarthick24@gmail.com>
2023-08-03 07:01:50 +0530

    palindrome.java is added

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+public class palindrome {
+    public static void main(String[] args)
+    {
+        int rev=0,rem,num=101;
+        int temp=num;
+        while(num!=0)
+        {
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        if(rev==temp)
+        {
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println(" not a Palindrome");
+        }
+    }
+    
+}
