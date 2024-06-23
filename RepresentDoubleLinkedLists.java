04589d2fb202a17c9e56fe3e1e1901d1487d1cc7
Vellaisamy-24 <vellaikarthick24@gmail.com>
2024-06-23 08:44:16 +0530

    basic rep of double linked list

---
+++ RepresentDoubleLinkedLists.java
@@ -0,0 +1,37 @@
+package DS;
+
+public class RepresentDoubleLinkedLists {
+    private Node head;
+    private Node tail;
+    private int length;
+    private class Node{
+        public int data;
+        public Node next;
+         public Node previous;
+        public Node(int data)
+        {
+            this.data=data;
+
+        }
+    }
+    public void DoubleList()
+    {
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+    }
+    public Boolean isEmpty()
+    {
+        return length==0;
+    }
+    public int length()
+    {
+        return length;
+    }
+    public static void main(String[] args)
+    {
+                      
+
+    }
+    
+}
